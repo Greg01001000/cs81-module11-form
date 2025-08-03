f24508ae73e8e49d8d0b11cca4198590245076a1
Greg Hagen <gr-a@outlook.com>
2025-08-02 23:31:41 -0700

    Added ContactForm.css, added GitHub link, project, and name to tops of key files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// https://github.com/Greg01001000/cs81-module11-form
+// CS 81 Module 11 Assignment 11A: React Contact Form, by GregH, 8/2/25
+
 import React from 'react';
 import ContactForm from './ContactForm';
 
--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,4 +1,8 @@
+// https://github.com/Greg01001000/cs81-module11-form
+// CS 81 Module 11 Assignment 11A: React Contact Form, by GregH, 8/2/25
+
 import React, { useState } from 'react';
+import './ContactForm.css';
 
 function ContactForm() {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
@@ -31,23 +35,30 @@ function ContactForm() {
   };
 
   return (
-    <div>
+    <div className="contact">
       <h2>Contact Us</h2>
       <form onSubmit={handleSubmit}>
-        <label>Name:<br />
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-          {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
-        </label><br /><br />
 
-        <label>Email:<br />
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
-          {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
-        </label><br /><br />
+        <div className="input-box">
+            <label>Name:<br />
+            <input type="text" className="field" name="name" value={formData.name} onChange={handleChange} placeholder='Enter your name' />
+            {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
+            </label><br /><br />
+        </div>
 
-        <label>Message:<br />
-          <textarea name="message" rows="5" value={formData.message} onChange={handleChange}></textarea>
-          {errors.message && <p style={{color: 'red'}}>{errors.message}</p>}
-        </label><br /><br />
+        <div className="input-box">
+            <label>Email:<br />
+            <input type="email" className="field" name="email" value={formData.email} onChange={handleChange} placeholder='Enter your email' />
+            {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
+            </label><br /><br />
+        </div>
+
+        <div className="input-box">
+            <label>Message:<br />
+            <textarea className="field mess" name="message" rows="5" value={formData.message} onChange={handleChange} placeholder='Enter your message'></textarea>
+            {errors.message && <p style={{color: 'red'}}>{errors.message}</p>}
+            </label><br /><br />
+        </div>
 
         <button type="submit">Submit</button>
       </form>
